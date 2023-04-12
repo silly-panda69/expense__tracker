12a0b6847586cfa2e38445f38746dc27c1682ae7
Skarma Singhay Yangs <102505459+silly-panda69@users.noreply.github.com>
2023-04-13 05:20:58 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -109,8 +109,8 @@ const HomePage = () => {
                         <h5 className='text-primary' >Recent Transactions</h5>
                         <Link to='/transactions' className='' style={{textDecorationLine: 'none'}}><h5><span className='badge bg-primary'>View All</span></h5></Link>
                     </div>
-                    <div className=' table-responsive-lg'>
-                        <table className='table table-sm table-borderless'>
+                    <div className='table-responsive-lg'>
+                    <table className='table table-sm table-borderless'>
                         <thead className='shadow-sm'>
                             <tr>
                                 <th scope='col'>Name</th>
