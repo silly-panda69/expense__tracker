7ed5eb5af7a49f830491cb4ff263bd00a7fcf32a
Skarma Singhay Yangs <102505459+silly-panda69@users.noreply.github.com>
2023-04-13 03:51:07 +0530

    Update Transactions.js

--- frontend/src/pages/Transactions.js
+++ frontend/src/pages/Transactions.js
@@ -4,6 +4,9 @@ import { ExpenseTotalContext } from '../contexts/ExpenseTotalContext';
 import { AuthContext } from '../contexts/AuthContext';
 import {format} from'date-fns';
 import Rupee from '../components/Rupee';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const Transactions = () => {
     const {data,dispatch1}=useContext(ExpenseContext);
@@ -54,8 +57,8 @@ const Transactions = () => {
             }
         });
         if(response.ok){
-            dispatch1({type: 'REMOVE_EXPENSE',payload: json._id});
             const json=await response.json();
+            dispatch1({type: 'REMOVE_EXPENSE',payload: json._id});
             if(json.flow==='Transaction'){
                 dispatch2({type: 'MINUS_TOTAL',payload: json.amount});
             }
@@ -157,7 +160,7 @@ const Transactions = () => {
                                     {item.flow==='Income' && <td><h6><span className='badge bg-success'>{item.flow}</span></h6></td>}
                                     <td >
                                         <div className="dropdown">
-                                            <button className='btn-sm drop-down-toggle'  id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                            <button className='btn btn-sm dropdown-toggle'  id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
   <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
 </svg></button>
                                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">                                          
